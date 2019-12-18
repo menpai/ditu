a9ac8d01aa17a4204da9deed6d55a5ac3745addf
vkiller <15703618+vkiller@users.noreply.github.com>
2019-12-18 07:15:21 +0000

    Deploying to gh-pages from master 5a254a5eb3973c3e91f272f4ac22db518f1e4aab

---
+++ precache-manifest.e89c8a1e98bb77d238a5e0b92a53eb2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5de7f03ed0bb9c52883a3f9d911e96e",
+    "url": "/ditu/index.html"
+  },
+  {
+    "revision": "e46e40ee28e04f4b3d26",
+    "url": "/ditu/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "ac5540120edbe2a4b142",
+    "url": "/ditu/static/js/2.359ad3e1.chunk.js"
+  },
+  {
+    "revision": "83076cb9ae51d6c2171ae41a5d00380c",
+    "url": "/ditu/static/js/2.359ad3e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e46e40ee28e04f4b3d26",
+    "url": "/ditu/static/js/main.7adab860.chunk.js"
+  },
+  {
+    "revision": "ce027b22341da7d138e8",
+    "url": "/ditu/static/js/runtime-main.dcb130d8.js"
+  },
+  {
+    "revision": "5dce9009578187b2803f3ab0bc44d8b0",
+    "url": "/ditu/static/media/map.5dce9009.csv"
+  }
+]);
