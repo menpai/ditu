053940b682018c3516eca5bab893a2a05211f842
vkiller <15703618+vkiller@users.noreply.github.com>
2019-12-18 06:29:18 +0000

    Deploying to release from master 52ceccd1dce4631c6df3e83bba3562e5cb2747df

---
+++ precache-manifest.841118980348a7f84974cc42266169e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08035532387072f8cd48083d195cf4e1",
+    "url": "/ditu/index.html"
+  },
+  {
+    "revision": "1f565d74f2a197912db9",
+    "url": "/ditu/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "ac5540120edbe2a4b142",
+    "url": "/ditu/static/js/2.359ad3e1.chunk.js"
+  },
+  {
+    "revision": "83076cb9ae51d6c2171ae41a5d00380c",
+    "url": "/ditu/static/js/2.359ad3e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f565d74f2a197912db9",
+    "url": "/ditu/static/js/main.ede21c20.chunk.js"
+  },
+  {
+    "revision": "ce027b22341da7d138e8",
+    "url": "/ditu/static/js/runtime-main.dcb130d8.js"
+  },
+  {
+    "revision": "5dce9009578187b2803f3ab0bc44d8b0",
+    "url": "/ditu/static/media/map.5dce9009.csv"
+  }
+]);
