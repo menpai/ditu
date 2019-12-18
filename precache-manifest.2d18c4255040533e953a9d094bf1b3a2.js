1a1cd090892ddead675094889cc1eb3b8f5d605a
vkiller <vkiller@users.noreply.github.com>
2019-12-18 08:02:41 +0000

    Automated deployment: Wed Dec 18 08:02:41 UTC 2019 26d674ecf93a66eaf39311e983e6a9c5cf32222b

---
+++ precache-manifest.2d18c4255040533e953a9d094bf1b3a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d96e845aa8df6f6fc06e2a5844d4692",
+    "url": "/ditu/index.html"
+  },
+  {
+    "revision": "a0223c217c99a47295f9",
+    "url": "/ditu/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "ac5540120edbe2a4b142",
+    "url": "/ditu/static/js/2.359ad3e1.chunk.js"
+  },
+  {
+    "revision": "83076cb9ae51d6c2171ae41a5d00380c",
+    "url": "/ditu/static/js/2.359ad3e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0223c217c99a47295f9",
+    "url": "/ditu/static/js/main.d69faca9.chunk.js"
+  },
+  {
+    "revision": "ce027b22341da7d138e8",
+    "url": "/ditu/static/js/runtime-main.dcb130d8.js"
+  },
+  {
+    "revision": "5dce9009578187b2803f3ab0bc44d8b0",
+    "url": "/ditu/static/media/map.5dce9009.csv"
+  }
+]);
